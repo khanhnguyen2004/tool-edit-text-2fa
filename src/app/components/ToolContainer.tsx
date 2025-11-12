9d436746e43389585645e6a49e537b26fdf34db5
khanhnguyen2004 <khanhntnpt00010@gmail.com>
2025-11-12 11:26:57 +0700

    Update

--- src/app/components/ToolContainer.tsx
+++ src/app/components/ToolContainer.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { useState } from 'react';
 import Sidebar from './SideBar';
+import ToolFacebookLink from './tools/ToolFacebookLink';
 
 export default function ToolContainer() {
     const [selectedTool, setSelectedTool] = useState('cookie');
@@ -9,10 +10,54 @@ export default function ToolContainer() {
         switch (selectedTool) {
             case 'cookie':
             // return <CookieTool />;
+            case 'pomodoro':
+            // code xử lý cho Pomodoro
             case 'edit-text':
             // return <EditTextTool />;
             case 'uid-to-year':
             // return <UidToCreatedYearTool />;
+            case 'cat-chuoi':
+
+            case 'cat-line':
+
+            case 'ghep-dong':
+
+            case 'trung-lap':
+
+            case 'dao-tu':
+
+            case 'ghep-chuoi':
+
+            case 'chia-cat-dong':
+
+            case 'loc-chuoi':
+
+            case 'loc-chu':
+
+            case 'copy-file':
+
+            case 'anh-html':
+
+            case 'link-html':
+
+            case 'loc-csv':
+
+            case 'ghep-file':
+
+            case 'json':
+
+            case 'loc-tag':
+
+            case 'tinh-sub':
+
+            case 'join-chu':
+
+            case 'account':
+
+            case 'loai-text':
+
+            case 'facebook-link':
+                return <ToolFacebookLink />
             default:
                 return <div className="p-6 text-gray-500 text-center">Đang phát triển...</div>;
         }
